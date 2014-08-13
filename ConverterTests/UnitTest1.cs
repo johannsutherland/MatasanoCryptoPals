0349534e1be50354e531c804ba15985ae2640696
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-08-13 22:05:18 +0100

    Challenge 1

---
+++ ConverterTests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matasano;
+
+namespace ConverterTests
+{
+    [TestClass]
+    public class ConverterTests
+    {
+        [TestMethod]
+        public void ConvertOneCharacter()
+        {
+            string hex = "68";
+            string base64 = "aA==";
+            Converter c = new Converter();
+            Assert.AreEqual(c.HexToBase64(hex), base64);
+        }
+
+        [TestMethod]
+        public void ConvertTwoCharactersWithHyphen()
+        {
+            string hex = "68-69";
+            string base64 = "aGk=";
+            Converter c = new Converter();
+            Assert.AreEqual(c.HexToBase64(hex), base64);
+        }
+
+        [TestMethod]
+        public void ConvertTwoCharactersWithoutHyphen()
+        {
+            string hex = "6869";
+            string base64 = "aGk=";
+            Converter c = new Converter();
+            Assert.AreEqual(c.HexToBase64(hex), base64);
+        }
+
+        [TestMethod]
+        public void Challenge1()
+        {
+            string hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+            string base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+            Converter c = new Converter();
+            Assert.AreEqual(c.HexToBase64(hex), base64);
+        }
+    }
+}
---
+++ HexToBase64/Converter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matasano
+{
+    public class Converter
+    {
+        public string HexToBase64(string hex)
+        {
+            string[] hexNumbers = GetHexNumbers(hex);
+            byte[] bytes = new byte[hexNumbers.Count()];
+            int n = 0;
+            foreach (string h in hexNumbers)
+            {
+                bytes[n++] = (byte)int.Parse(h, System.Globalization.NumberStyles.HexNumber);
+            }
+            return Convert.ToBase64String(bytes, Base64FormattingOptions.None);
+        }
+
+        private string[] GetHexNumbers(string hex)
+        {
+            if (hex.Contains('-'))
+                return hex.Split('-');
+
+            string[] h = new string[hex.Length / 2];
+            int n = 0;
+            while (n*2 < hex.Length)
+            {
+                h[n] = hex.Substring(n*2, 2);
+                n++;
+            }
+            return h;
+        }
+    }
+}
