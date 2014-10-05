d35af30a17a146d0e1c30d8dd877470bf9d622a8
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-10-05 22:10:52 +0100

    Set 2 Challenge 13

---
+++ ConverterTests/ProfileManagerTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matasano;
+using System.Collections.Generic;
+
+namespace ConverterTests
+{
+    [TestClass]
+    public class CookieTest
+    {
+        [TestMethod]
+        public void CookieParserFromString()
+        {
+            var cookie = "foo=bar&baz=qux&zap=zazzle";
+
+            Cookie cookieParser = new Cookie(cookie);
+
+            Assert.AreEqual(cookieParser["foo"], "bar");
+            Assert.AreEqual(cookieParser["baz"], "qux");
+            Assert.AreEqual(cookieParser["zap"], "zazzle");
+        }
+
+        [TestMethod]
+        public void CookieParserFromInvalidString()
+        {
+            var cookie = "foo=bar=1&baz=qux&zap=zazzle";
+
+            Cookie cookieParser = new Cookie(cookie);
+
+            try
+            {
+                var value = cookieParser["foo"];
+                Assert.Fail("Key should not exist");
+            }
+            catch (KeyNotFoundException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Invalid exception");
+            }
+            
+            Assert.AreEqual(cookieParser["baz"], "qux");
+            Assert.AreEqual(cookieParser["zap"], "zazzle");
+        }
+
+        [TestMethod]
+        public void CookieToString()
+        {
+            var str = "foo=bar&baz=qux&zap=zazzle";
+
+            Cookie cookie = new Cookie(str);
+
+            Assert.AreEqual(str, cookie.ToString());
+        }
+
+        [TestMethod]
+        public void BreakProfileManager()
+        {
+            ProfileManager profileManger = new ProfileManager();
+            string userRoleEmail = "foooo@bar.com";
+
+            ProfileManagerCracker pmc = new ProfileManagerCracker(profileManger);
+            pmc.BreakProfileManager(userRoleEmail);
+
+            Assert.AreEqual("admin", profileManger[userRoleEmail]["role"]);
+        }
+    }
+}
---
+++ HexToBase64/ProfileManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matasano
+{
+    public class Cookie
+    {
+        private Dictionary<string, string> values = new Dictionary<string, string>();
+
+        public Cookie(string cookie)
+        {
+            cookie.Split('&').ToList().ForEach(token =>
+                {
+                    var kvp = token.Split('=');
+                    if (kvp.Length == 2) values.Add(kvp[0], kvp[1]);
+                });
+        }
+
+        public string this[string key]
+        {
+            get { return values[key]; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            values.ToList().ForEach(kvp => sb.Append(kvp.Key + "=" + kvp.Value + "&"));
+            string result = sb.ToString();
+            return result.Substring(0, result.Length - 1);
+        }
+    }
+
+    public class ProfileManager
+    {
+        private Dictionary<string, Cookie> profiles = new Dictionary<string, Cookie>();
+        AESCipher cipher = new AESCipher();
+        AESCipherHelper helper = new AESCipherHelper();
+
+        string defaultProfile = "&uid=10&role=user";
+        string key;
+
+        public ProfileManager()
+        {
+            key = helper.GenerateKey().ToString();
+        }
+
+        public void AddProfile(string email)
+        {
+            string santisedEmail = email.Replace("&", "").Replace("=", "");
+            Cookie cookie = new Cookie("email=" + santisedEmail + defaultProfile);
+
+            profiles.Add(santisedEmail, cookie);
+        }
+
+        public void AddProfile(string email, string encoded)
+        {
+            string decoded = cipher.DecryptECB(key, encoded);
+
+            if (profiles.ContainsKey(email))
+            {
+                profiles[email] = new Cookie(decoded);
+            }
+            else
+            {
+                profiles.Add(email, new Cookie(decoded));
+            }
+        }
+
+        public Cookie this[string email]
+        {
+            get { return profiles[email]; }
+        }
+
+        public string Encrypted(string email)
+        {
+            return cipher.EncryptECB(profiles[email].ToString()).Decode();
+        }
+    }
+}
