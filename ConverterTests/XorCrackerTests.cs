18f4fbce7f2c307aafa6fb253de92f5183468c07
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-10-06 22:16:11 +0100

    Moved Xor.txt

--- ConverterTests/XorCrackerTests.cs
+++ ConverterTests/XorCrackerTests.cs
@@ -14,7 +14,7 @@ namespace ConverterTests
         {
             XorCracker cracker = new XorCracker();
             string expected = "Terminator X: Bring the noise";
-            Base64 data = new Base64(String.Join("", File.ReadAllLines(@"Xor.txt")));
+            Base64 data = new Base64(String.Join("", File.ReadAllLines(@"TestFiles\Xor.txt")));
             string[] result = cracker.BreakXorFile(data, 2, 60, 4);
             Assert.AreEqual(expected, result[0]);
         }
