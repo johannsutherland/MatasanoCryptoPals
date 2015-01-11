42735f9a9c72b9b3cae1b0b6068062f4a7232a54
Johann Sutherland <johannsutherland@yahoo.co.uk>
2015-01-11 19:19:06 +0000

    SubmissionManager initial check in

---
+++ HexToBase64/SubmissionManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matasano
+{
+    class SubmissionManager
+    {
+        AESCipher cipher = new AESCipher();
+        AESCipherHelper helper = new AESCipherHelper();
+
+        string prepend = "comment1=cooking%20MCs;userdata=";
+        string append = ";comment2=%20like%20a%20pound%20of%20bacon";
+        string key;
+        string iv;
+
+        public SubmissionManager()
+        {
+            key = helper.GenerateKey().ToString();
+            iv = new String('\0', 16);
+        }
+
+        public Base64 Encrypt(string userInput)
+        {
+            string sanitisedUserInput = userInput.Replace(";", String.Empty).Replace("=", String.Empty);
+            string padded = prepend + sanitisedUserInput + append;
+
+            return cipher.EncryptCBC(key, padded, iv);
+        }
+
+        public bool IsAdmin(Base64 encrypted)
+        {
+            string values = cipher.DecryptCBC(key, encrypted, iv);
+            ValuePairParser dictionaryManager = new ValuePairParser(values, ';');
+            return dictionaryManager["admin"] == "true";
+        }
+    }
+}
