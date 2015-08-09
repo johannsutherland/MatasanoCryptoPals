97eee3e79a74ef03979792676721ed0edd79eb68
Johann Sutherland <johannsutherland@yahoo.co.uk>
2015-08-09 22:37:29 +0100

    Add all methods to interface

--- Solutions/Oracle/IEncryptionOracle.cs
+++ Solutions/Oracle/IEncryptionOracle.cs
@@ -1,7 +1,10 @@
-﻿namespace Matasano.Oracle
+﻿using System;
+
+namespace Matasano.Oracle
 {
     public interface IEncryptionOracle
     {
         Base64 EncryptConsistentKey(string data, Base64 unknownString, int startIndex = 0);
+        Tuple<Base64, string> EncryptWithRandomPadding(string data);
     }
 }
