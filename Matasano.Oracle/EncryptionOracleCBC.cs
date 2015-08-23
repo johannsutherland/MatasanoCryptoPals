9b0569542b4d71ed5953605cf77a83c4e1f7fcca
Johann Sutherland <johannsutherland@yahoo.co.uk>
2015-08-23 21:59:21 +0100

    Missing files

---
+++ Matasano.Oracle/EncryptionOracleCBC.cs
@@ -0,0 +1,39 @@
+﻿using Matasano.Cipher.AES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matasano.Oracle
+{
+    public class EncryptionOracleCBC
+    {
+        Random random = new Random();
+        AESCipherCBC cipherCBC = new AESCipherCBC();
+        AESCipherHelper helper = new AESCipherHelper();
+        private string key;
+
+        public EncryptionOracleCBC()
+        {
+            key = helper.GenerateKey().ToString();
+        }
+
+        public Tuple<Base64, string> Encrypt(string data)
+        {
+            string iv = helper.GenerateKey().ToString();
+
+            return new Tuple<Base64, string>(cipherCBC.Encrypt(key, data, iv), iv);
+        }
+
+        public string Decrypt(Hex data, string iv)
+        {
+            return cipherCBC.Decrypt(key, data, iv);
+        }
+
+        public bool IsValidPadding(Hex data, string iv)
+        {
+            return cipherCBC.IsValidPadding(key, data, iv);
+        }
+    }
+}
